69428669001847e9311194598c55f49b2b1fe6c1
Sasidhar Attaluri <attaluri@adobe.com>
2017-11-16 21:13:00 +0530

    Added Book model, Book Admin class and changed Store template

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from store.models import Book
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author','price','stock')
+
+
+admin.site.register(Book,BookAdmin)
--- store/views.py
+++ store/views.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from store.models import Book
 
 
 def index(request):
     return render(request, 'template.html')
 
+
 def store(request):
-    return render(request,'store.html')
+    count = Book.objects.all().count()
+    context = {
+        'count':count,
+    }
+    return render(request,'store.html',context)
 
