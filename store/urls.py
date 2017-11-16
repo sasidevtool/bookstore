5dd844f688ab7c47235b4fa0b630b1de49da8b46
Sasidhar Attaluri <attaluri@adobe.com>
2017-11-16 17:00:20 +0530

    Add store, views, urls

---
+++ store/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from views import store
+
+urlpatterns = (
+    url(r'^$', store, name='index'),
+)
+
---
+++ store/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'template.html')
+
+def store(request):
+    return render(request,'store.html')
+
